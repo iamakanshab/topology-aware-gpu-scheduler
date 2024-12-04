bd03e75b8d0f1ebf2c578e1c6e7bc1b34a09c244
Akansha Bansal <akansha.bansal@amd.com>
2024-12-04 14:45:00 -0800

    Update main.go

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -17,8 +17,9 @@ import (
     "k8s.io/kubernetes/pkg/scheduler/apis/config"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "github.com/prometheus/client_golang/prometheus/promhttp"
-    "github.com/iamakanshab/topology-aware-scheduler/pkg/scheduler/algorithm"
-    clientset "github.com/iamakanshab/topology-aware-scheduler/pkg/generated/clientset/versioned"
+
+    "github.com/yourusername/topology-aware-gpu-scheduler/pkg/scheduler/algorithm"
+    clientset "github.com/yourusername/topology-aware-gpu-scheduler/pkg/generated/clientset/versioned"
 )
 
 var (
