1e4e54f228f6cee8073aec06b16c415c601a6861
Akansha Bansal <akansha.bansal@amd.com>
2024-12-04 15:02:19 -0800

    Update codegen.sh

--- codegen.sh
+++ codegen.sh
@@ -4,16 +4,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+# Ensure GOPATH is set
+GOPATH=$(go env GOPATH)
+if [ -z "${GOPATH}" ]; then
+    echo "GOPATH is not set"
+    exit 1
+fi
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/iamakanshab/topology-aware-gpu-scheduler/pkg/generated \
-  github.com/iamakanshab/topology-aware-gpu-scheduler/pkg/apis \
-  topology:v1alpha1 \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+PROJECT_ROOT="$SCRIPT_ROOT"
+MODULE_NAME="topology-aware-gpu-scheduler"
+
+# Ensure the code-generator module is downloaded
+go get -d k8s.io/code-generator@v0.28.0
+CODEGEN_PKG="${GOPATH}/pkg/mod/k8s.io/code-generator@v0.28.0"
+
+echo "Generating client codes..."
+bash "${CODEGEN_PKG}/generate-groups.sh" "deepcopy,client,informer,lister" \
+  ${MODULE_NAME}/pkg/generated \
+  ${MODULE_NAME}/pkg/apis \
+  "topology:v1alpha1" \
+  --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
+  --output-base "${PROJECT_ROOT}"
+
+# The generated code will be in the wrong location, so we need to move it
+mv "${PROJECT_ROOT}/${MODULE_NAME}/pkg/generated" "${PROJECT_ROOT}/pkg/"
+rm -rf "${PROJECT_ROOT}/${MODULE_NAME}"
