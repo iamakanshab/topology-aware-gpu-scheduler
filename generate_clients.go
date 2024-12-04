96747a94d7ea3cdca9c344cc4e8ba79396b9ea68
Akansha Bansal <akansha.bansal@amd.com>
2024-12-04 12:09:48 -0800

    gen

--- generate_clients.go
+++ generate_clients.go
@@ -1,45 +1,47 @@
-//go:build generate
 // +build generate
 
 package main
 
 import (
-	"fmt"
-	"path/filepath"
+    "fmt"
+    "os"
+    "path/filepath"
 
-	"k8s.io/klog/v2"
+    "k8s.io/code-generator/cmd/client-gen"
+    clientgenargs "k8s.io/code-generator/cmd/client-gen/args"
+    "k8s.io/code-generator/cmd/informer-gen"
+    informergenargs "k8s.io/code-generator/cmd/informer-gen/args"
+    "k8s.io/klog/v2"
 )
 
 func main() {
-	// Paths
-	apisPath := filepath.Join("pkg", "apis")
-	clientsetPath := filepath.Join("pkg", "generated", "clientset")
-	informersPath := filepath.Join("pkg", "generated", "informers")
+    // Paths
+    apisPath := filepath.Join("pkg", "apis")
+    clientsetPath := filepath.Join("pkg", "generated", "clientset")
+    informersPath := filepath.Join("pkg", "generated", "informers")
 
-	// Generate clientset
-	if err := client_gen.Run(
-		&client_gen.Generator{
-			InputVersions:   []string{"topology/v1alpha1"},
-			InputPackages:   []string{apisPath},
-			OutputPackage:   clientsetPath,
-			BoilerplatePath: "boilerplate.go.txt",
-		},
-	); err != nil {
-		klog.Fatalf("Failed to generate clientset: %v", err)
-	}
+    // Generate clientset
+    clientGenArgs := &clientgenargs.GeneratorArgs{
+        InputPackages:    []string{apisPath + "/topology/v1alpha1"},
+        OutputPackage:    clientsetPath,
+        ClientSetName:    "versioned",
+        BoilerplateFile:  "boilerplate.go.txt",
+    }
+    if err := client_gen.Run(clientGenArgs); err != nil {
+        klog.Fatalf("Failed to generate clientset: %v", err)
+    }
 
-	// Generate informers
-	if err := informer_gen.Run(
-		&informer_gen.GeneratorArgs{
-			VersionedClientSetPackage: fmt.Sprintf("%s/versioned", clientsetPath),
-			InternalClientSetPackage:  fmt.Sprintf("%s/internalclientset", clientsetPath),
-			ListerPackage:             fmt.Sprintf("%s/listers", informersPath),
-			InformerPackage:           fmt.Sprintf("%s/externalversions", informersPath),
-			InputDirectories:          []string{apisPath + "/topology/v1alpha1"},
-			OutputPackagePath:         informersPath,
-			BoilerplatePath:           "boilerplate.go.txt",
-		},
-	); err != nil {
-		klog.Fatalf("Failed to generate informers: %v", err)
-	}
+    // Generate informers
+    informerGenArgs := &informergenargs.GeneratorArgs{
+        VersionedClientSetPackage: fmt.Sprintf("%s/versioned", clientsetPath),
+        InternalClientSetPackage:  fmt.Sprintf("%s/internalclientset", clientsetPath),
+        ListerPackage:             fmt.Sprintf("%s/listers", informersPath),
+        InformerPackage:           fmt.Sprintf("%s/externalversions", informersPath),
+        InputDirectories:          []string{apisPath + "/topology/v1alpha1"},
+        OutputPackagePath:         informersPath,
+        BoilerplateFile:           "boilerplate.go.txt",
+    }
+    if err := informer_gen.Run(informerGenArgs); err != nil {
+        klog.Fatalf("Failed to generate informers: %v", err)
+    }
 }
