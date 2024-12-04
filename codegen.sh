783a846541aa77c1c12dac5e53ed97632fbf8d88
Akansha Bansal <akansha.bansal@amd.com>
2024-12-04 14:48:29 -0800

    Update codegen.sh

--- codegen.sh
+++ codegen.sh
@@ -1,25 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-# Ensure the go modules are downloaded
-go mod download
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-# Define your module name
-MODULE="your.module/name"
-
-# Run the generators
-go run k8s.io/code-generator/cmd/client-gen \
-  --input-dirs ${MODULE}/pkg/apis/topology/v1alpha1 \
-  --output-package ${MODULE}/pkg/generated/clientset \
-  --clientset-name versioned \
-  --go-header-file boilerplate.go.txt
-
-go run k8s.io/code-generator/cmd/informer-gen \
-  --input-dirs ${MODULE}/pkg/apis/topology/v1alpha1 \
-  --versioned-clientset-package ${MODULE}/pkg/generated/clientset/versioned \
-  --listers-package ${MODULE}/pkg/generated/listers \
-  --output-package ${MODULE}/pkg/generated/informers \
-  --go-header-file boilerplate.go.txt
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/iamakanshab/topology-aware-gpu-scheduler/pkg/generated \
+  github.com/iamakanshab/topology-aware-gpu-scheduler/pkg/apis \
+  topology:v1alpha1 \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
