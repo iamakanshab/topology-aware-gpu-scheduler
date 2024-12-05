91ee56e4a7815f666bdce3cc258e635222a5f910
Akansha Bansal <akansha.bansal@amd.com>
2024-12-04 16:13:13 -0800

    Update types.go

--- pkg/apis/topology/v1alpha1/types.go
+++ pkg/apis/topology/v1alpha1/types.go
@@ -1,8 +1,13 @@
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:defaulter-gen=true
 package v1alpha1
 
 import (
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
 
 // +genclient
 // +genclient:noStatus
