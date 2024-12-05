2f8e63be6e805cb5bc0ad56c04b7440ab7577e67
Akansha Bansal <akansha.bansal@amd.com>
2024-12-04 16:23:20 -0800

    Update codegen.sh

--- codegen.sh
+++ codegen.sh
@@ -8,6 +8,9 @@ ROOT_DIR=$(pwd)
 CODEGEN_PKG=$(go env GOPATH)/pkg/mod/k8s.io/code-generator@v0.28.0
 API_PKG_PATH="github.com/iamakanshab/topology-aware-gpu-scheduler"
 
+echo "Cleaning up previously generated code..."
+rm -rf pkg/generated/*
+
 # Create a temporary build directory with proper permissions
 TEMP_DIR=$(mktemp -d)
 chmod 755 "${TEMP_DIR}"
@@ -24,10 +27,39 @@ chmod -R 755 "${TEMP_DIR}"
 cd "${TEMP_DIR}/src/${API_PKG_PATH}"
 
 echo "Running code generator..."
-GOPATH="${TEMP_DIR}" GO111MODULE=on "${CODEGEN_PKG}/generate-groups.sh" \
-  "client,informer,lister,deepcopy" \
-  "${API_PKG_PATH}/pkg/generated" \
-  "${API_PKG_PATH}/pkg/apis" \
+
+# Generate deepcopy
+${CODEGEN_PKG}/generate-groups.sh \
+  "deepcopy" \
+  ${API_PKG_PATH}/pkg/generated \
+  ${API_PKG_PATH}/pkg/apis \
+  "topology:v1alpha1" \
+  --output-base "${TEMP_DIR}/src" \
+  --go-header-file "${PWD}/boilerplate.go.txt"
+
+# Generate client
+${CODEGEN_PKG}/generate-groups.sh \
+  "client" \
+  ${API_PKG_PATH}/pkg/generated \
+  ${API_PKG_PATH}/pkg/apis \
+  "topology:v1alpha1" \
+  --output-base "${TEMP_DIR}/src" \
+  --go-header-file "${PWD}/boilerplate.go.txt"
+
+# Generate informers
+${CODEGEN_PKG}/generate-groups.sh \
+  "informer" \
+  ${API_PKG_PATH}/pkg/generated \
+  ${API_PKG_PATH}/pkg/apis \
+  "topology:v1alpha1" \
+  --output-base "${TEMP_DIR}/src" \
+  --go-header-file "${PWD}/boilerplate.go.txt"
+
+# Generate listers
+${CODEGEN_PKG}/generate-groups.sh \
+  "lister" \
+  ${API_PKG_PATH}/pkg/generated \
+  ${API_PKG_PATH}/pkg/apis \
   "topology:v1alpha1" \
   --output-base "${TEMP_DIR}/src" \
   --go-header-file "${PWD}/boilerplate.go.txt"
@@ -44,3 +76,4 @@ if [ -d "${TEMP_DIR}" ]; then
 fi
 
 echo "Code generation complete!"
+ls -la pkg/generated/
